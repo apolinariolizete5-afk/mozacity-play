7626b1dab9b81656ca254604ea4b7d1480ed20ea
apolinariolizete5-afk <apolinariolizete5@gmail.com>
2026-09-26 14:13:15 +0200

    Remove external web push implementation

--- src/lib/push.functions.ts
+++
@@ -1,5 +0,0 @@
-import { createServerFn } from "@tanstack/react-start";
-
-export const getVapidPublicKey = createServerFn({ method: "GET" }).handler(async () => ({
-  publicKey: process.env["VAPID_PUBLIC_KEY"] ?? "",
-}));
